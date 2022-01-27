d123bf43b52a79762d644db32a3211a9efd6819f
KatiaKuroski <katiamkuroski@gmail.com>
2022-01-27 08:52:59 -0300

    origin

--- cypress/integration/SeguradoGerarBoleto.js
+++ cypress/integration/SeguradoGerarBoleto.js
@@ -7,7 +7,7 @@
                 cy.get('#onetrust-accept-btn-handler').click();
                 cy.get('#loginDropdown > .nav-link__label').click();
                 cy.get('.btn-login-cliente').click();
-                cy.get('#login-cliente__input').focus().type('769.746.853-00');
+                cy.get('#login-cliente__input').focus().type('295.200.518-48');
                 cy.get('.login-cliente > .login-form > .has-show-hide-password > .form-control').focus().type('Nov@1234');
                 cy.get('.login-cliente > .login-form > .btn').click();
                 cy.get('.list-icons',{timeout:5000}).scrollIntoView();
@@ -28,34 +28,37 @@
             cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .input-options > :nth-child(2) > .celularBoleto > #celularBoleto').type("41996059283");
             cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .bt-options > .bt-primary').click();
             cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .result-sendform > .bt-options > #proximaParcela').click();
+          
+
+
             cy.get('.nav-link.d-none > .nav-link > span').click();
         });
-        // it("Selecionando o Cliente e a Parcela a Vencer - Boleto", () => {
-        //     cy.visit("https://wwwdsvn2.azulseguros.com.br/");
+    //      it("Selecionando o Cliente e a Parcela a Vencer - Boleto", () => {
+    //          cy.visit("https://wwwdsvn2.azulseguros.com.br/");
            
-        //     cy.get('#onetrust-accept-btn-handler').click();
-        //     cy.get('#loginDropdown > .nav-link__label').click();
-        //     cy.get('.btn-login-cliente').click();
-        //     cy.get('#login-cliente__input').focus().type('415.298.200-49');
-        //     cy.get('.login-cliente > .login-form > .has-show-hide-password > .form-control').focus().type('Nov@1234');
-        //     cy.get('.login-cliente > .login-form > .btn').click();
-        //     cy.get('.list-icons',{timeout:5000}).scrollIntoView();
-        //     cy.get('[data-gtm-name="relacao-de-parcelas').click();
+    //          cy.get('#onetrust-accept-btn-handler').click();
+    //          cy.get('#loginDropdown > .nav-link__label').click();
+    //          cy.get('.btn-login-cliente').click();
+    //          cy.get('#login-cliente__input').focus().type('415.298.200-49');
+    //          cy.get('.login-cliente > .login-form > .has-show-hide-password > .form-control').focus().type('Nov@1234');
+    //          cy.get('.login-cliente > .login-form > .btn').click();
+    //         cy.get('.list-icons',{timeout:5000}).scrollIntoView();
+    //         cy.get('[data-gtm-name="relacao-de-parcelas').click();
            
             
-        // cy.log ("gerar boleto");
-        // cy.get('img[title="Gerar Boleto"]', {timeout:40000}).first().click();
-        // //selecione as opções
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(1) > .left-opt > #copyCode').click();
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(1) > :nth-child(2) > #imprimirBoleto').click();
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(2) > .left-opt > #enviarPorEmail').click();
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .input-options > .left-opt > .emailBoleto > #emailBoleto').type("katiakuroski.opentec@azulseguros.com.br");
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(2) > :nth-child(2) > #receberSMS').click();
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .input-options > :nth-child(2) > .celularBoleto > #celularBoleto').type("41996059283");
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .bt-options > .bt-primary').click();
+    //    cy.log ("gerar boleto");
+    //     cy.get('img[title="Gerar Boleto"]', {timeout:40000}).first().click();
+    //   selecione as opções
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(1) > .left-opt > #copyCode').click();
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(1) > :nth-child(2) > #imprimirBoleto').click();
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(2) > .left-opt > #enviarPorEmail').click();
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .input-options > .left-opt > .emailBoleto > #emailBoleto').type("katiakuroski.opentec@azulseguros.com.br");
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .actions-width > :nth-child(2) > :nth-child(2) > #receberSMS').click();
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .input-options > :nth-child(2) > .celularBoleto > #celularBoleto').type("41996059283");
+    //     cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .bt-options > .bt-primary').click();
        
-        // //cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .result-sendform > .bt-options > #proximaParcela').click();
-        // cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .bt-options > .bt-primary').click();
-       // cy.get('.nav-link.d-none > .nav-link > span').click();
+    //    cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .result-sendform > .bt-options > #proximaParcela').click();
+    //    cy.get('.block-ui-container-inner > :nth-child(1) > .content-modal-boleto > .form-boleto > .popup-actions > .bt-options > .bt-primary').click();
+    //    cy.get('.nav-link.d-none > .nav-link > span').click();
   //  });
 });
